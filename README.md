5bd5c746708c423a3a0739483cd935c8b53adbbe
Raphaël Renaudon <raphael@onlc.fr>
2022-03-12 18:12:19 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# anchorremotecontrol
+# Anchor Remote Control
+![In-page navigation script](https://static.onlc.eu/anchorrcNDD/164710071339.png)
+
 Wherever you are on a webpage, a remote control follows you and helps you navigate throw the anchors. It is a simple way to navigate within a long webpage.
+
+[**🌍  Website**](https://arc.onlinecreation.pro)
+
+## How to add to my website
+
+1. If you already use Bootstrap 5, that awesome, you don't need any CSS. Else, download [anchorremotecontrol.nobootstrap.css](https://static.onlc.eu/scripts/anchorremotecontrol/0.0.1/anchorremotecontrol.nobootstrap.css) and include it to your website.
+
+```html
+<link rel="stylesheet" src="anchorremotecontrol.nobootstrap.css" />
+```
+
+2. This CSS adds common Bootstrap utilities. It might have side effects on your website.
+3. Download [anchorremotecontrol.js](https://static.onlc.eu/scripts/anchorremotecontrol/0.0.1/anchorremotecontrol.js).
+4. Upload it to your website.
+5. Add this just before the closing body tag:
+```html
+<script src="anchorremotecontrol.js"></script>
+```
+6. Add a different id attribute to each of your heading tags (h1, h2, h3, h4 and h5). If a heading already has an id attribute, but you don't want it in the ARC, add data-arc-ignore to the heading.
+
+You might also direct link us:
+```html
+<!-- Insert this just before your </body> -->
+<script src="https://static.onlc.eu/scripts/anchorremotecontrol/0.0.1/anchorremotecontrol.js"></script>
+
+<!-- If you don't use Bootstrap 5, insert this befire your </head> -->
+<link rel="stylesheet" src="https://static.onlc.eu/scripts/anchorremotecontrol/0.0.1/anchorremotecontrol.nobootstrap.css" />
+```
+
+## FAQ
+
+### How to customize the active color?
+
+  If you use Bootstrap, the active color is the main color of your theme. The quick and dirty way :
+  ```html
+  <style>
+    .anchorremotecontrol .list-group-item.active {
+      background-color: #125F00;
+      border-color: #125F00;
+    }
+  </style>
+  ```
+  If you don't use Bootstrap, you can change the active color by defining the CSS var --arc-active-color before calling anchorremotecontrol.nobootstrap.css.
+  ```html
+  <style>
+    :root{--arc-active-color: #125f00;}
+  </style>
+  <link rel="stylesheet" src="https://static.onlc.eu/scripts/anchorremotecontrol/0.0.1/anchorremotecontrol.nobootstrap.css" />
+  ```
+
+### When I scroll to a heading, it scrolls too far and my heading is hidden. How can I fix this?
+
+Just add this before calling anchorremotecontrol.js:
+```html
+<script>
+  let headingTopMargin = 100;
+</script>
+<script src="https://static.onlc.eu/scripts/anchorremotecontrol/0.0.1/anchorremotecontrol.js"></script>
+```
+Adapt the *100* to your needs.
+
+## About
+
+Anchor remote control is a project by [Online Creation](https://www.onlinecreation.pro), a web agency based in France.
+They are also the creators of [OnlineCreation.me](https://www.onlinecreation.me), the easiest website creation platform ever made.
+
+## License
+
+This project is licensed under the MIT license.
+
+## Support
+
+Nope, sorry. But this is simple code, I'm sure you'll handle it ! :)
+
+If you are completely lost, try to file an issue on GitHub, maybe someone will help you.
---
+++ anchorremotecontrol.js
@@ -0,0 +1,170 @@
+
+let headingsPos = [];
+let headings;
+let topMargin = 0;
+
+/**
+ * Run a smooth scroll to the target position
+ * @param {number} ypos Target position
+ */
+function smoothScrollTo(ypos) {
+  if (ypos < 0) {
+    ypos = 0;
+  }
+  window.scroll({
+    top: ypos,
+    behavior: "smooth"
+  });
+}
+
+/**
+ * Get the position of the target element and run a smooth scroll to it
+ * @param {HTMLElement} e Target element
+ */
+function scroller(e) {
+  e.preventDefault();
+  smoothScrollTo(e.target.getAttribute('data-pos') - topMargin);
+}
+
+/**
+ * Find the element closest to the current viewport top
+ */
+function scrollEvent() {
+  const closest = headingsPos.filter(function(pos) {
+    return pos >= window.scrollY && pos <= window.scrollY + window.innerHeight;
+  }).sort(function(a, b){return a-b});
+  const active = document.querySelectorAll('.anchorremotecontrol-list a.active');
+  for (let i = 0; i < active.length; i++) {
+    active[i].classList.remove('active');
+  }
+  document.querySelector('.anchorremotecontrol-list a[data-pos="' + closest[0] + '"]').classList.add('active');
+}
+
+/**
+ * Add the anchorremotecontrol to the page
+ */
+function plantSeed() {
+  let isClosed = '';
+  if (localStorage.getItem('anchorremotecontrol') === 'closed') {
+    isClosed = 'closed'
+  } else if (window.innerWidth < 768) {
+    isClosed = 'closed'
+  }
+  const seed = `
+<style>
+  .anchorremotecontrol > div {
+    z-index: 99999;
+  }
+  .anchorremotecontrol .anchorremotecontrol-container {
+    overflow-y: auto;
+    overflow-x: hidden;
+    max-height: calc(100vh - 1.5em);
+    max-width: 300px;
+    transition: all 0.5s ease;
+  }
+  .anchorremotecontrol .openclose {
+    z-index: 99999;
+    width: 1em;
+    height: 1em;
+    line-height: 1em;
+    box-sizing: content-box;
+    transition: all 0.5s ease;
+    cursor: pointer;
+  }
+  .anchorremotecontrol.closed .openclose {
+    transform: rotate(180deg);
+  }
+  .anchorremotecontrol.closed .anchorremotecontrol-container {
+    transform: translateX(400px);
+  }
+</style>
+<div class="anchorremotecontrol ` + isClosed + `">
+  <div class="position-fixed top-50 translate-middle-y end-0 rounded me-2">
+    <div class="text-end">
+      <div class="openclose p-2 mb-2 rounded-circle bg-white shadow-sm d-inline-block text-center">
+        »
+      </div>
+    </div>
+    <div class="shadow-sm anchorremotecontrol-container">
+      <div class="list-group anchorremotecontrol-list">
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+  document.body.innerHTML += seed;
+}
+
+/**
+ * Find the relevent headings and add them to the list
+ * @returns {Array} Array of headings
+ */
+function findHeadings() {
+  const query = 'h1[id]:not([data-arc-ignore]), h2[id]:not([data-arc-ignore]), h3[id]:not([data-arc-ignore]), h4[id]:not([data-arc-ignore]), h5[id]:not([data-arc-ignore])';
+  return document.querySelectorAll(query);
+}
+
+/**
+ * Create one item in the list
+ * @param {string} title Heading title
+ * @param {string} anchor Anchor
+ * @param {number} pos Heading position
+ * @returns {HTMLElement}
+ */
+function createAnchor(title, anchor, pos) {
+  let a = document.createElement('a');
+  a.setAttribute('href', '#' + anchor);
+  a.setAttribute('class', 'list-group-item list-group-item-action text-nowrap text-truncate');
+  a.setAttribute('data-pos', pos);
+  headingsPos.push(pos);
+  a.innerText = title;
+  a.addEventListener('click', scroller, false);
+  return a;
+}
+
+/**
+ * Create the list
+ */
+function createAnchors() {
+  headings = findHeadings();
+  const topAnchor = createAnchor('↑', 'top', 0);
+  document.querySelector('.anchorremotecontrol-list').appendChild(topAnchor);
+  for (var i = 0; i < headings.length; i++) {
+    const heading = headings[i];
+    const headingId = heading.getAttribute('id');
+    const headingText = heading.innerText;
+    const pos = Math.round(headings[i].getBoundingClientRect().top) + window.scrollY;
+    const anchor = createAnchor(headingText, headingId, pos)
+    document.querySelector('.anchorremotecontrol-list').appendChild(anchor);
+  }
+}
+
+/**
+ * Initialize the anchorremotecontrol open/close button
+ */
+function initOpenClose() {
+  document.querySelector('.anchorremotecontrol .openclose').addEventListener('click', function() {
+    const arc = document.querySelector('.anchorremotecontrol');
+    arc.classList.toggle('closed');
+    localStorage.setItem('anchorremotecontrol', arc.classList.contains('closed') ? 'closed' : 'open');
+  });
+  if (localStorage.getItem('anchorremotecontrol') === 'closed') {
+    document.querySelector('.anchorremotecontrol').classList.add('closed');
+  }
+}
+
+/**
+ * Initialize the anchorremotecontrol
+ */
+function init() {
+  if (typeof headingTopMargin !== 'undefined') {
+    topMargin = headingTopMargin;
+  }
+  plantSeed();
+  createAnchors();
+  initOpenClose();
+  scrollEvent();
+  window.addEventListener('scroll', scrollEvent, false);
+}
+
+window.addEventListener('load', init, false);
